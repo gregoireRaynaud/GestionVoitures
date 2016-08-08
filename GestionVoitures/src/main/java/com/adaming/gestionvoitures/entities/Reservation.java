f4d24da2d8bb77bb1de40dd3e5117ed76298abc5
inti0215 <inti0215@INTI-0215>
2016-08-08 17:24:59 +0200

    commit entitées Voiture et Reservation

--- GestionVoitures/src/main/java/com/adaming/gestionvoitures/entities/Reservation.java
+++ GestionVoitures/src/main/java/com/adaming/gestionvoitures/entities/Reservation.java
@@ -1,5 +1,134 @@
 package com.adaming.gestionvoitures.entities;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/*
+ * Classe : Reservation
+ * Type : entity
+ * Groupe : Adaming
+ * Author : Gwladys Ledocq
+ * Date : 08/08/2016
+ * Version : 1.0.0
+ */
+
+@Entity
 public class Reservation {
 
+	/*Attributs*/
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idreservation;
+	private double prixReservation;
+	@Temporal(TemporalType.TIME)
+	private Date dateReservation;//juste date
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dateRetour;//date + heure
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dateSortie;//date + heure
+	private Integer nombresDeJours;
+	private String etatReservation;
+	
+	/*Association*/
+	
+	@OneToMany(mappedBy="tabReservations")
+	@JoinColumn(name="idVoiture")
+	private Voiture voiture;
+
+	/*Constructeurs*/
+	
+	public Reservation() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Reservation(double prixReservation, Date dateReservation,
+			Date dateRetour, Date dateSortie, Integer nombresDeJours,
+			String etatReservation) {
+		super();
+		this.prixReservation = prixReservation;
+		this.dateReservation = dateReservation;
+		this.dateRetour = dateRetour;
+		this.dateSortie = dateSortie;
+		this.nombresDeJours = nombresDeJours;
+		this.etatReservation = etatReservation;
+	}
+	
+	/*Getters and setters*/
+
+	public Long getIdreservation() {
+		return idreservation;
+	}
+
+	public void setIdreservation(Long idreservation) {
+		this.idreservation = idreservation;
+	}
+
+	public double getPrixReservation() {
+		return prixReservation;
+	}
+
+	public void setPrixReservation(double prixReservation) {
+		this.prixReservation = prixReservation;
+	}
+
+	public Date getDateReservation() {
+		return dateReservation;
+	}
+
+	public void setDateReservation(Date dateReservation) {
+		this.dateReservation = dateReservation;
+	}
+
+	public Date getDateRetour() {
+		return dateRetour;
+	}
+
+	public void setDateRetour(Date dateRetour) {
+		this.dateRetour = dateRetour;
+	}
+
+	public Date getDateSortie() {
+		return dateSortie;
+	}
+
+	public void setDateSortie(Date dateSortie) {
+		this.dateSortie = dateSortie;
+	}
+
+	public Integer getNombresDeJours() {
+		return nombresDeJours;
+	}
+
+	public void setNombresDeJours(Integer nombresDeJours) {
+		this.nombresDeJours = nombresDeJours;
+	}
+
+	public String getEtatReservation() {
+		return etatReservation;
+	}
+
+	public void setEtatReservation(String etatReservation) {
+		this.etatReservation = etatReservation;
+	}
+
+	public Voiture getVoiture() {
+		return voiture;
+	}
+
+	public void setVoiture(Voiture voiture) {
+		this.voiture = voiture;
+	}
+	
+	
+	
 }
--- GestionVoitures/src/main/java/com/adaming/gestionvoitures/entities/Voiture.java
+++ GestionVoitures/src/main/java/com/adaming/gestionvoitures/entities/Voiture.java
@@ -1,5 +1,141 @@
 package com.adaming.gestionvoitures.entities;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+
+import org.springframework.context.annotation.Lazy;
+
+/*
+ * Classe : Voiture
+ * Type : entity
+ * Groupe : Adaming
+ * Author : Gwladys Ledocq
+ * Date : 08/08/2016
+ * Version : 1.0.0
+ */
+
+@Entity
 public class Voiture {
 
+	/*Attributs*/
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idvoiture;
+    private String modelVoiture;
+    private String immatricule;
+    private Double kilometrage;
+    private Double prixVoiture;
+    private String typeVoiture;
+    private String typeCarburant;
+    private String etatVoiture;
+    
+    /*Associations*/
+    
+    @ManyToOne
+    @JoinTable(name="Voiture_Reservation")
+    private List<Reservation> tabReservations;
+
+    /*Constructeurs*/
+    
+	public Voiture() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Voiture(String modelVoiture, String immatricule, Double kilometrage,
+			Double prixVoiture, String typeVoiture, String typeCarburant,
+			String etatVoiture) {
+		super();
+		this.modelVoiture = modelVoiture;
+		this.immatricule = immatricule;
+		this.kilometrage = kilometrage;
+		this.prixVoiture = prixVoiture;
+		this.typeVoiture = typeVoiture;
+		this.typeCarburant = typeCarburant;
+		this.etatVoiture = etatVoiture;
+	}
+	
+	/*Getters and setters*/
+
+	public Long getIdvoiture() {
+		return idvoiture;
+	}
+
+	public void setIdvoiture(Long idvoiture) {
+		this.idvoiture = idvoiture;
+	}
+
+	public String getModelVoiture() {
+		return modelVoiture;
+	}
+
+	public void setModelVoiture(String modelVoiture) {
+		this.modelVoiture = modelVoiture;
+	}
+
+	public String getImmatricule() {
+		return immatricule;
+	}
+
+	public void setImmatricule(String immatricule) {
+		this.immatricule = immatricule;
+	}
+
+	public Double getKilometrage() {
+		return kilometrage;
+	}
+
+	public void setKilometrage(Double kilometrage) {
+		this.kilometrage = kilometrage;
+	}
+
+	public Double getPrixVoiture() {
+		return prixVoiture;
+	}
+
+	public void setPrixVoiture(Double prixVoiture) {
+		this.prixVoiture = prixVoiture;
+	}
+
+	public String getTypeVoiture() {
+		return typeVoiture;
+	}
+
+	public void setTypeVoiture(String typeVoiture) {
+		this.typeVoiture = typeVoiture;
+	}
+
+	public String getTypeCarburant() {
+		return typeCarburant;
+	}
+
+	public void setTypeCarburant(String typeCarburant) {
+		this.typeCarburant = typeCarburant;
+	}
+
+	public String getEtatVoiture() {
+		return etatVoiture;
+	}
+
+	public void setEtatVoiture(String etatVoiture) {
+		this.etatVoiture = etatVoiture;
+	}
+
+	public List<Reservation> getTabReservations() {
+		return tabReservations;
+	}
+
+	public void setTabReservations(List<Reservation> tabReservations) {
+		this.tabReservations = tabReservations;
+	}
+    
+	
 }
