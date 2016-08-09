95b78b3736cd5494af26d38888d745fab3b3d58a
inti0215 <inti0215@INTI-0215>
2016-08-09 12:38:33 +0200

    commit vide

--- GestionVoitures/src/main/java/com/adaming/gestionvoitures/service/agence/AgenceServiceImpl.java
+++ GestionVoitures/src/main/java/com/adaming/gestionvoitures/service/agence/AgenceServiceImpl.java
@@ -3,6 +3,7 @@ package com.adaming.gestionvoitures.service.agence;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
