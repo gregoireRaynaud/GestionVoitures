c83770db52e84b959a1ee715c2bfe6df7491ba7e
GautierFolgoas <gautier.folgoas@gmail.com>
2016-08-09 11:10:42 +0200

    Commit de facture dao et service

--- GestionVoitures/src/main/java/com/adaming/gestionvoitures/dao/facture/IFactureDao.java
+++ GestionVoitures/src/main/java/com/adaming/gestionvoitures/dao/facture/IFactureDao.java
@@ -1,5 +1,16 @@
 package com.adaming.gestionvoitures.dao.facture;
 
+import java.util.List;
+
+import com.adaming.gestionvoitures.entities.Facture;
+
 public interface IFactureDao {
+	
+	public Facture addFacture(Facture f, Long idReservation, Long idAgence);
+	public Facture getFactureById(Long idFacture);
+	public List<Facture> getListFactures();
+	public Double calculerCoutFacture(Long idFacture);
+	public Double calculerCoutFacturesByClient(Long idClient);
+	public Double calculerCoutFacturesByVoiture(Long idVoiture);
 
 }
--- GestionVoitures/src/main/java/com/adaming/gestionvoitures/service/facture/IFactureService.java
+++ GestionVoitures/src/main/java/com/adaming/gestionvoitures/service/facture/IFactureService.java
@@ -1,5 +1,16 @@
 package com.adaming.gestionvoitures.service.facture;
 
+import java.util.List;
+
+import com.adaming.gestionvoitures.entities.Facture;
+
 public interface IFactureService {
+	
+	public Facture addFacture(Facture f, Long idReservation, Long idAgence);
+	public Facture getFactureById(Long idFacture);
+	public List<Facture> getListFactures();
+	public Double calculerCoutFacture(Long idFacture);
+	public Double calculerCoutFacturesByClient(Long idClient);
+	public Double calculerCoutFacturesByVoiture(Long idVoiture);
 
 }
