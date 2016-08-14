5a0be43585381c8685949c12fd4e0827ecfc6dd1
GautierFolgoas <gautier.folgoas@gmail.com>
2016-08-14 21:09:26 +0200

    commit des xhtml de facture/agence, facture/agence bean, et de faces config avec quelques lignes lignes en plus pour gérer le requestContext de FactureBean

---
+++ GestionVoitures/src/main/java/com/adaming/gestionvoitures/bean/AgenceBean.java
@@ -0,0 +1,225 @@
+package com.adaming.gestionvoitures.bean;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import java.util.List;
+
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.adaming.gestionvoitures.entities.Agence;
+
+import com.adaming.gestionvoitures.service.agence.IAgenceService;
+
+
+@Component("AgenceBean")
+@Scope("session")
+public class AgenceBean {
+	
+	@Autowired
+	private IAgenceService agenceService;
+	
+	private Long idAgence;
+	private String addrespostal;
+	private String numerotel;
+	private String denomination;
+	private String formJuridique;
+	private String activite;
+	private String adressSiege;
+	private Date dateCommencement;
+	private String ville;
+	private String deuxiemTel;
+	private Double tva;
+	private String choixMonnais;
+	private List<String> listMonnaie = new ArrayList<String>();
+	
+	private Agence agence;
+	
+	private List<Agence> tabAgences;
+	private List<Agence> tabAgencesFiltered;
+	
+	/*METHODES*/
+	@PostConstruct
+	public void remplirListMonnaie(){
+		listMonnaie.add("Dollar");
+		listMonnaie.add("Euro");
+		listMonnaie.add("Livre-Sterling");
+		listMonnaie.add("Dinar");
+		listMonnaie.add("Yen");
+	}
+	
+	public void addAgence(){
+		Agence a = new Agence(addrespostal, numerotel, denomination, formJuridique, activite, adressSiege, dateCommencement, ville, deuxiemTel, tva, choixMonnais);
+		agenceService.addAgence(a);
+		addMessage("Agence enregistrée!");
+	}
+	public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+	public void getAgenceByIdBean(){
+		agence = agenceService.getAgenceById(idAgence);
+		
+		idAgence = agence.getIdAgence();
+		addrespostal = agence.getAddrespostal();
+		numerotel = agence.getNumerotel();
+		denomination = agence.getDenomination();
+		formJuridique = agence.getFormJuridique();
+		activite = agence.getActivite();
+		adressSiege = agence.getAdressSiege();
+		dateCommencement = agence.getDateCommencement();
+		ville = agence.getVille();
+		deuxiemTel = agence.getDeuxiemTel();
+		tva = agence.getTva();
+		choixMonnais = agence.getChoixMonnais();			
+	}
+	public void updateAgence(){
+		Agence a = new Agence(addrespostal, numerotel, denomination, formJuridique, activite, adressSiege, dateCommencement, ville, deuxiemTel, tva, choixMonnais);
+		agenceService.updateAgence(a);
+		addMessage("Agence modifiée!");
+	}
+	@PostConstruct
+	public void getAgences(){
+		tabAgences = agenceService.getAgences();
+	}
+	@PostConstruct
+	public String redirectPage(){		
+		return "facture";
+	}
+	
+	/*GETTERS & SETTERS*/
+	
+	public Long getIdAgence() {
+		return idAgence;
+	}
+	
+	public void setIdAgence(Long idAgence) {
+		this.idAgence = idAgence;
+	}
+
+	public String getAddrespostal() {
+		return addrespostal;
+	}
+
+	public void setAddrespostal(String addrespostal) {
+		this.addrespostal = addrespostal;
+	}
+
+	public String getNumerotel() {
+		return numerotel;
+	}
+
+	public void setNumerotel(String numerotel) {
+		this.numerotel = numerotel;
+	}
+
+	public String getDenomination() {
+		return denomination;
+	}
+
+	public void setDenomination(String denomination) {
+		this.denomination = denomination;
+	}
+
+	public String getFormJuridique() {
+		return formJuridique;
+	}
+
+	public void setFormJuridique(String formJuridique) {
+		this.formJuridique = formJuridique;
+	}
+
+	public String getActivite() {
+		return activite;
+	}
+
+	public void setActivite(String activite) {
+		this.activite = activite;
+	}
+
+	public String getAdressSiege() {
+		return adressSiege;
+	}
+
+	public void setAdressSiege(String adressSiege) {
+		this.adressSiege = adressSiege;
+	}
+
+	public Date getDateCommencement() {
+		return dateCommencement;
+	}
+
+	public void setDateCommencement(Date dateCommencement) {
+		this.dateCommencement = dateCommencement;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getDeuxiemTel() {
+		return deuxiemTel;
+	}
+
+	public void setDeuxiemTel(String deuxiemTel) {
+		this.deuxiemTel = deuxiemTel;
+	}
+
+	public Double getTva() {
+		return tva;
+	}
+
+	public void setTva(Double tva) {
+		this.tva = tva;
+	}
+
+	public String getChoixMonnais() {
+		return choixMonnais;
+	}
+
+	public void setChoixMonnais(String choixMonnais) {
+		this.choixMonnais = choixMonnais;
+	}
+
+	public List<Agence> getTabAgences() {
+		return tabAgences;
+	}
+
+	public void setTabAgences(List<Agence> tabAgences) {
+		this.tabAgences = tabAgences;
+	}
+	public Agence getAgence() {
+		return agence;
+	}
+	public void setAgence(Agence agence) {
+		this.agence = agence;
+	}
+	public List<Agence> getTabAgencesFiltered() {
+		return tabAgencesFiltered;
+	}
+	public void setTabAgencesFiltered(List<Agence> tabAgencesFiltered) {
+		this.tabAgencesFiltered = tabAgencesFiltered;
+	}
+
+	public List<String> getListMonnaie() {
+		return listMonnaie;
+	}
+
+	public void setListMonnaie(List<String> listMonnaie) {
+		this.listMonnaie = listMonnaie;
+	}	
+	
+
+}
---
+++ GestionVoitures/src/main/java/com/adaming/gestionvoitures/bean/FactureBean.java
@@ -0,0 +1,275 @@
+package com.adaming.gestionvoitures.bean;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.validation.constraints.NotNull;
+
+import org.primefaces.context.RequestContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.adaming.gestionvoitures.entities.Agence;
+import com.adaming.gestionvoitures.entities.Facture;
+import com.adaming.gestionvoitures.entities.Reservation;
+import com.adaming.gestionvoitures.service.agence.IAgenceService;
+import com.adaming.gestionvoitures.service.facture.IFactureService;
+import com.adaming.gestionvoitures.service.reservation.IReservationService;
+
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@Component("FactureBean")
+@RequestScoped
+public class FactureBean {
+	
+	@Autowired
+	private IFactureService factureService;
+	@Autowired
+	private IReservationService reservationService;
+	@Autowired
+	private IAgenceService agenceService;
+	
+	private Long idFacture;
+	private Long idFacture1;
+	@NotNull(message="Veuillez entrer une date de facturation.")
+	private Date deFacturation;
+	private List<Facture> tabFactures;
+	private Facture facture;
+	
+	private Double coutFacture;
+	private Double coutFacturesByClient;
+	private Double coutFacturesByVoiture;
+	
+	private Long idReservation;
+	private Long idAgence;
+	private Long idClient;
+	private Long idVoiture;
+	private Reservation reservation;
+	private Agence agence;
+	private List<Reservation> tabReservations;
+	private List<Agence> tabAgences;
+	
+	/*METHODES*/
+	public void addFacture() throws ObjectNotFoundException{
+		Facture f = new Facture(deFacturation);
+		factureService.addFacture(f, idReservation, idAgence);
+		addMessage("Facture enregistrée!");
+	}
+	public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+	
+	public void getFactureById(){
+		facture = factureService.getFactureById(idFacture);
+		calculerCoutFacture();
+				
+		/*Appel une autre page xhtml avec options de présentations dans un hashmap*/
+		Map<String,Object> options = new HashMap<String, Object>();
+		options.put("resizable", false);
+		options.put("modal", true);
+        options.put("width", 680);
+        options.put("height", 570);
+        RequestContext.getCurrentInstance().openDialog("FactureToPrint", options, null);
+		
+	}     
+    
+	@PostConstruct
+	public void getListFactures(){
+		tabFactures = factureService.getListFactures();
+	}
+	
+	public void calculerCoutFacture(){
+		coutFacture = factureService.calculerCoutFacture(idFacture);
+	}
+	
+	public void calculerCoutFacturesByClient(){
+		coutFacturesByClient = factureService.calculerCoutFacturesByClient(idClient);
+	}
+	
+	public void calculerCoutFacturesByVoiture(){
+		coutFacturesByVoiture = factureService.calculerCoutFacturesByVoiture(idVoiture);
+	}
+	@PostConstruct
+	public void getListReservation(){
+		tabReservations = reservationService.getReservations();
+	}
+	@PostConstruct
+	public void getListAgences(){
+		tabAgences = agenceService.getAgences();
+	}
+	@PostConstruct
+	public String redirectPage(){
+		idFacture = null;
+		deFacturation = null;
+		tabFactures = null;
+		facture = null;
+		
+		coutFacture = null;
+		coutFacturesByClient = null;
+		coutFacturesByVoiture = null;
+		
+		idReservation = null;
+		idAgence = null;
+		idClient = null;
+		idVoiture = null;
+		reservation = null;
+		agence = null;
+		tabReservations = null;
+		tabAgences = null;
+		
+		return "facture";
+	}
+
+	/*GETTERS & SETTERS*/
+	public IFactureService getfactureService() {
+		return factureService;
+	}
+
+	public void setfactureService(IFactureService factureService) {
+		this.factureService = factureService;
+	}
+
+	public Long getIdFacture() {
+		return idFacture;
+	}
+
+	public void setIdFacture(Long idFacture) {
+		this.idFacture = idFacture;
+	}
+
+	public Date getDeFacturation() {
+		return deFacturation;
+	}
+
+	public void setDeFacturation(Date deFacturation) {
+		this.deFacturation = deFacturation;
+	}
+
+	public List<Facture> getTabFactures() {
+		return tabFactures;
+	}
+
+	public void setTabFactures(List<Facture> tabFactures) {
+		this.tabFactures = tabFactures;
+	}
+
+	public Long getIdReservation() {
+		return idReservation;
+	}
+
+	public void setIdReservation(Long idReservation) {
+		this.idReservation = idReservation;
+	}
+
+	public Long getIdAgence() {
+		return idAgence;
+	}
+
+	public void setIdAgence(Long idAgence) {
+		this.idAgence = idAgence;
+	}
+
+	public Facture getFacture() {
+		return facture;
+	}
+
+	public void setFacture(Facture facture) {
+		this.facture = facture;
+	}
+
+	public Double getCoutFacture() {
+		return coutFacture;
+	}
+
+	public void setCoutFacture(Double coutFacture) {
+		this.coutFacture = coutFacture;
+	}
+
+	public Double getCoutFacturesByClient() {
+		return coutFacturesByClient;
+	}
+
+	public void setCoutFacturesByClient(Double coutFacturesByClient) {
+		this.coutFacturesByClient = coutFacturesByClient;
+	}
+
+	public Double getCoutFacturesByVoiture() {
+		return coutFacturesByVoiture;
+	}
+
+	public void setCoutFacturesByVoiture(Double coutFacturesByVoiture) {
+		this.coutFacturesByVoiture = coutFacturesByVoiture;
+	}
+
+	public Long getIdClient() {
+		return idClient;
+	}
+
+	public void setIdClient(Long idClient) {
+		this.idClient = idClient;
+	}
+
+	public Long getIdVoiture() {
+		return idVoiture;
+	}
+
+	public void setIdVoiture(Long idVoiture) {
+		this.idVoiture = idVoiture;
+	}
+
+	public List<Reservation> getTabReservations() {
+		return tabReservations;
+	}
+
+	public void setTabReservations(List<Reservation> tabReservations) {
+		this.tabReservations = tabReservations;
+	}
+
+	public List<Agence> getTabAgences() {
+		return tabAgences;
+	}
+
+	public void setTabAgences(List<Agence> tabAgences) {
+		this.tabAgences = tabAgences;
+	}
+
+	public IAgenceService getAgenceService() {
+		return agenceService;
+	}
+
+	public void setAgenceService(IAgenceService agenceService) {
+		this.agenceService = agenceService;
+	}
+
+	public Agence getAgence() {
+		return agence;
+	}
+
+	public void setAgence(Agence agence) {
+		this.agence = agence;
+	}
+
+	public Reservation getReservation() {
+		return reservation;
+	}
+
+	public void setReservation(Reservation reservation) {
+		this.reservation = reservation;
+	}
+	public Long getIdFacture1() {
+		return idFacture1;
+	}
+	public void setIdFacture1(Long idFacture1) {
+		this.idFacture1 = idFacture1;
+	}
+	
+	
+}
+
