7a68dfee15cdaffb2b424d65336f5b53dce63781
GautierFolgoas <gautier.folgoas@gmail.com>
2016-08-22 16:46:18 +0200

    commit de l'exception ReservationDejaFacturee

---
+++ GestionVoitures/src/main/java/com/adaming/gestionvoitures/exception/ReservationDejaFacturee.java
@@ -0,0 +1,11 @@
+package com.adaming.gestionvoitures.exception;
+
+@SuppressWarnings("serial")
+public class ReservationDejaFacturee extends Exception {
+
+	public ReservationDejaFacturee(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
