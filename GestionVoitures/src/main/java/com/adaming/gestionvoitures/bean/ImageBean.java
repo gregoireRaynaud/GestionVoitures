8c7ad764374129a80a87445efafbdc1a69bf0111
elouesse <elouesse@hotmail.com>
2016-08-23 16:07:00 +0200

    Index, panneau d'images

---
+++ GestionVoitures/src/main/java/com/adaming/gestionvoitures/bean/ImageBean.java
@@ -0,0 +1,30 @@
+package com.adaming.gestionvoitures.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+/**
+ * Nom : ImageBean
+ * package com.adaming.gestionvoitures.bean;
+ * Classe pour gérer la gallerie d'images de la page d'accueil
+ * @author Grégoire RAYNAUD
+ * Date : 23/08/2016
+ */
+public class ImageBean {
+	private List<String> images;
+    
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 5; i++) {
+            images.add("voiture" + i + ".jpg");
+        }
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+}
