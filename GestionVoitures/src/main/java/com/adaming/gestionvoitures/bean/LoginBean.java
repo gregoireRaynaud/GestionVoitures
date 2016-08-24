10123764852eaa5004f76985eacf144291fb4422
GautierFolgoas <gautier.folgoas@gmail.com>
2016-08-24 16:31:40 +0200

    Commit de spring-security: - Web.xml - pom.xml - LoginBean - Login.whtml - app.xml

---
+++ GestionVoitures/src/main/java/com/adaming/gestionvoitures/bean/LoginBean.java
@@ -0,0 +1,84 @@
+package com.adaming.gestionvoitures.bean;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.web.WebAttributes;
+
+
+@SuppressWarnings("serial")
+@ManagedBean(name = "LoginBean")
+@RequestScoped
+public class LoginBean implements PhaseListener{
+	
+	Logger log = Logger.getLogger("LoginBean");
+	
+	private String username;
+	private String password;
+	
+    
+    public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String doLogin() throws IOException, ServletException {    
+		ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        RequestDispatcher dispatcher = ((ServletRequest) context.getRequest()).getRequestDispatcher("/j_spring_security_check");
+        dispatcher.forward((ServletRequest) context.getRequest(), (ServletResponse) context.getResponse());
+        FacesContext.getCurrentInstance().responseComplete();
+        return null;
+    }
+
+	@Override
+	public void afterPhase(PhaseEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforePhase(PhaseEvent event) {
+		Exception e = (Exception) FacesContext.getCurrentInstance().
+        getExternalContext().getSessionMap().get(WebAttributes.AUTHENTICATION_EXCEPTION);
+        if (e instanceof BadCredentialsException) {
+            log.info("Found exception in session map: "+e);
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(WebAttributes.AUTHENTICATION_EXCEPTION, null);
+        FacesContext.getCurrentInstance().addMessage(null,
+        		new FacesMessage(FacesMessage.SEVERITY_ERROR,
+            "Username or password not valid.", "Username or password not valid"));
+        }
+		
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+		return PhaseId.RENDER_RESPONSE;
+	}
+ 
+}
